f7b6999a0c6bc27236e88f6ca5567494f5aff2fe
yujiando11 <penandrisu602@gmail.com>
2023-11-23 12:15:01 +0900

    Improve divide

---
+++ test_divide_zero.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 yujiando11
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng()
+{
+	echo NG LINE ${1}
+	res=1
+}
+
+res=0
+
+out=$(cat num_zero | ./divide)
+[ "${out}" = "" ] || ng "${LINENO}"
+
+[ "${res}" = 0 ] && echo OK
+
+exit ${res}
+
+
